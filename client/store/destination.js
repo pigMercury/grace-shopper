b77faad525c396f9b3c5155ad9b5b23796afc185
Kara Garvey <karagarvey123@gmail.com>
2019-01-09 17:35:44 -0500

    created destination sub-reducer

---
+++ client/store/destination.js
@@ -0,0 +1,57 @@
+import axios from 'axios'
+
+//action types
+export const GOT_DESTINATIONS = 'GOT_DESTINATIONS'
+export const GOT_SINGLE_DESTINATION = 'GOT_SINGLE_DESTINATION'
+
+//action creators
+export const gotDestinations = destinations => {
+  return {
+    type: GOT_DESTINATIONS,
+    payload: destinations
+  }
+}
+
+export const gotSingleDestination = singleDestination => {
+  return {
+    type: GOT_SINGLE_DESTINATION,
+    payload: singleDestination
+  }
+}
+
+//thunks
+export const fetchDestinations = () => {
+  return async dispatch => {
+    const {data} = await axios.get('/api/destination')
+    dispatch(gotDestinations(data))
+  }
+}
+
+export const fetchSingleDestination = destinationId => {
+  return async dispatch => {
+    const {data} = await axios.get(`/api/destination/${destinationId}`)
+    dispatch(gotSingleDestination(data))
+  }
+}
+
+const initialState = {
+  destinations: [],
+  singleDestination: {}
+}
+
+const destinationsReducer = (state = initialState, action) => {
+  const newState = {...state}
+
+  switch (action.type) {
+    case GOT_DESTINATIONS:
+      newState.destinations = action.payload
+      return newState
+    case GOT_SINGLE_DESTINATION:
+      newState.singleDestination = action.payload
+      return newState
+    default:
+      return state
+  }
+}
+
+export default destinationsReducer
