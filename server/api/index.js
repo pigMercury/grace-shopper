646eaa9ebb610dc0986116a74543febe28585cb5
Sara Dornblaser <saradornblaser@gmail.com>
2019-01-09 12:33:05 -0500

    fixed typo

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/destinations', require('./destinations'));
+router.use('/destination', require('./destination'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
