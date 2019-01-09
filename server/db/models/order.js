056656f3cb48cf3e61f260f0018adfcab23cc740
Sara Dornblaser <saradornblaser@gmail.com>
2019-01-09 13:01:32 -0500

    added trip model

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,13 +1,9 @@
-const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-  numPassengers: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
   completed: {
+    defaultValue: false,
     type: Sequelize.BOOLEAN,
     allowNull: false
   }
