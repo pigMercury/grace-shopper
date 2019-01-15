6e4d95ee578816e9a9161b2203d1e4bfc28817cb
Sara Dornblaser <saradornblaser@gmail.com>
2019-01-15 12:15:04 -0500

    fixed last model test and cart cost total cost display

--- server/db/models/trip.spec.js
+++ server/db/models/trip.spec.js
@@ -4,15 +4,31 @@ const {expect} = require('chai')
 const db = require('../index')
 const Trip = db.model('trip')
 
-///////WHHHHYYYYY is it only running the first half of my tests?????
-
 describe('Trip model', () => {
   beforeEach(() => {
     return db.sync({force: true})
   })
 
+  describe('prevents invalid numPassengers', () => {
+    let badTrip
+
+    //returns error, as expected
+    beforeEach(async () => {
+      badTrip = await Trip.create({
+        numPassengers: -1
+      })
+    })
+
+    describe('invalid number', () => {
+      xit('does not populate numPassengers', () => {
+        expect(badTrip).to.be.equal(undefined)
+      })
+    })
+  })
+
   describe('Applies numPassengers', () => {
     let myTrip
+
     beforeEach(async () => {
       myTrip = await Trip.create({
         numPassengers: 5
@@ -22,22 +38,6 @@ describe('Trip model', () => {
     describe('valid number', () => {
       it('trips have passengers', () => {
         expect(myTrip.numPassengers).to.be.equal(5)
-      })
-    })
-  })
-
-  describe('prevents invalid numPassengers', () => {
-    let badTrip
-
-    beforeEach(async () => {
-      badTrip = await Trip.create({
-        numPassengers: -1
-      })
-
-      describe('invalid number', () => {
-        it('does not populate numPassengers', () => {
-          expect(badTrip.numPassengers).to.be.equal(undefined)
-        })
       })
     })
   })
